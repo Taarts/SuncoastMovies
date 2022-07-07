de110c3cf84131bdac6d5a862e4787fc8d4c7e22
Tricia Amheiser <tricia@taarts.net>
2022-07-07 11:45:31 -0400

    Created Movie.cs/SCMovies.Context.cs/C# to count

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Linq;
 
 namespace SuncoastMovies
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            var context = new SuncoastMoviesContext();
+
+            var movieCount = context.Movies.Count();
+            Console.WriteLine($"There are {movieCount} movies!");
+        }
     }
-  }
 }
